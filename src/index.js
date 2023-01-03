d2b35a40f7a15ea033e8ae66bfc961aaf8612cf3
AnmZ <anmit.ray911@gmail.com>
2023-01-03 09:00:03 +0530

    added routers to complete create api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyparser = require('body-parser')
 const {PORT} = require('./config/serverconfig');
+
 const CityRepository = require('./repository/city-repository');
+
+const Apiroutes = require('./routes/index');
 const setupAndStartServer = async() => 
 {
 
@@ -9,7 +12,7 @@ const setupAndStartServer = async() =>
 
     app.use(bodyparser.json());
     app.use(bodyparser.urlencoded({extended:true}));
-
+    app.use('/api',Apiroutes);
     app.listen(PORT, ()=>{
         console.log(`Server Started at Port ${PORT}`);
         })
